f10741973777658be08f10fba86325a85e96f0b2
wenglin1 <wenglin@buffalo.edu>
2020-03-18 10:48:08 -0400

    everything but checkbox is done"

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,76 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+  state = {
+    transactions: [],
+    searchTerm: ""
+  }
+
+  componentDidMount = () => {
+    fetch("http://localhost:6001/transactions")
+    .then(r => r.json())
+    .then(array => {
+      this.setState({
+        transactions: array
+      })
+    })
+  }
+
+  changeTerm = (string) => {
+    this.setState({
+      searchTerm: string
+    })
+  }
+
+  newFilteredArray = () => {
+    let {searchTerm, transactions} = this.state
+    let filteredArray = transactions.filter(t => {
+      return t.description.toLowerCase().includes(searchTerm)
+    })
+    return filteredArray
+  }
+
+  addTransaction = (newTransaction) => {
+    fetch("http://localhost:6001/transactions", {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newTransaction)
+    })
+    .then(r => r.json())
+    .then(result => {
+      let newArray = [...this.state.transactions, result]
+      this.setState({
+        transactions: newArray
+      })
+    })
+  }
+
+  deleteTransaction = (objID) => {
+    fetch(`http://localhost:6001/transactions/${objID}`, {
+      method: "DELETE"
+    })
+    .then(r => r.json())
+    .then(() => {
+      let newArray = this.state.transactions.filter(t => {
+        return t.id !== objID
+      })
+      this.setState({
+        transactions: newArray
+      })
+    })
+
+  }
+  
   render() {
+
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search searchTerm={this.state.searchTerm} changeTerm={this.changeTerm}/>
+        <AddTransactionForm addTransaction={this.addTransaction}/>
+        <TransactionsList transactions={this.newFilteredArray()} deleteTransaction={this.deleteTransaction}/>
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,19 +1,40 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+
+  state = {
+    date: "mm/dd/yyyy",
+    description: "",
+    category: "",
+    amount: "",
+  }
+
+  handleAllInput = (e) => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.addTransaction(this.state)
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.handleSubmit}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" value={this.state.date} onChange={this.handleAllInput}/>
+            <input type="text" name="description" placeholder="Description" value={this.state.description} onChange={this.handleAllInput}/>
+            <input type="text" name="category" placeholder="Category" value={this.state.category} onChange={this.handleAllInput} />
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              value={this.state.amount}
+              onChange={this.handleAllInput}
             />
           </div>
           <button className="ui button" type="submit">
